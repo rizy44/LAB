84a1920e82751cd03087f7ee527ec10e3e9de6ca
rizy44 <144576622+rizy44@users.noreply.github.com>
2024-03-14 00:31:47 +0700

    Add files via upload

---
+++ Lab1-PhanSo/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "PhanSo1.h"
+using namespace std;
+int main()
+{
+	TinhToan LetDoIt; 
+	LetDoIt.SetGiaTriHaiPhanSo();
+	LetDoIt.Tong();
+	LetDoIt.Hieu();
+	LetDoIt.Tich();
+	LetDoIt.Thuong();
+	LetDoIt.SoSanh();
+	return 0;
+}
